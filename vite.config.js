f8b82b966fbdb1ab58ecdc86f4646ef46e909ce4
Soumyojit Choudhury <soumyojitchoudhury@gmail.com>
2025-06-28 11:54:40 +0530

    deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import ghPages from 'vite-plugin-gh-pages'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),ghPages()],
-  base: '/SIMON-GAME/',
+  plugins: [react(),tailwindcss()],
+  base: "/SIMON-GAME",
 })
